4b7e7801d7eeb4825de7cfe423b3629ce06f3078
BenicioNGdev <beniciong.dev@gmail.com>
2026-04-09 15:24:53 -0300

    Delete Radar.py

--- Radar.py
+++
@@ -1,8 +0,0 @@
-velocidade = int(input("Digite a velocidade: "))
-
-if velocidade > 110:
-    excesso = velocidade - 110
-    multa = excesso * 5
-    print(f"Multado! Valor: R$ {multa}")
-else:
-    print("Dentro do limite")
